d76bebdb5f18dd55523b67ad4c5c54da6af6746e
MateuszRuczaj1 <MateuszRuczaj1>
2025-10-17 18:01:46 +0200

    changed colors a bit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
-const name = "Mateusz"
-const title = "Crypto wallet"
+   import { ref } from 'vue';
+  const title =  ref("Crypto wallet")
+  const name = ref("Mateusz Ruczaj")
+  const showLoginForm = ref(false)
+  function hello(){
+       showLoginForm.value = !showLoginForm.value
+  }
 </script>
 
 <template>
   <header>
     <p class="title">{{ title }}</p>  
-    <p class="name">Witaj, {{ name }}</p>
+    <div class="flex items-center justify-between max-md:w-full gap-5">
+      <p class="name">Witaj, {{ name }}</p>
+    <button class="px-12 py-2 bg-[#1f2948] hover:bg-[#28355c] hover:cursor-pointer hover:-translate-y-0.5 transition-all duration-150 rounded-xl text-white hover:shadow-amber-200 hover:shadow-md" @click="hello">Zaloguj</button>
+  
+    </div>
   </header>
 
-  <main>
+  <main class="flex-1 flex items-center py-2">
+    <p v-if="showLoginForm" class="text-white font-bold mx-auto">Logowanie</p>
   </main>
 </template>
 
@@ -20,14 +30,23 @@ header {
   justify-content: space-between;
   align-items: center;
   padding: 1.5rem 2rem;
-  border-radius: 12px;
-  background-color: #430a3e;
-  
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  background-color: #151d32;
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
 }
+@media (max-width: 768px){
+  header{
+    flex-direction: column;
+    align-items: start;
+    gap: 2rem;
+  }
+}
+
 header p {
   color: #fff;
   margin: 0;
+  align-self: center;
 }
 .title {
   font-size: 1.8rem;
