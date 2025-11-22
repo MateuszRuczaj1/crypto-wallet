d18baf4bae09fcc513bce6a17c06594d75a2bf40
MateuszRuczaj1 <matrucz@outlook.com>
2025-11-22 14:48:05 +0100

    add user structure

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserSchema } from 'src/schemas/user.schema';
