79b83997ad860eb00c602469f17bd5049e82332f
MateuszRuczaj1 <MateuszRuczaj1>
2025-12-02 21:45:01 +0100

    update crypto schema props

--- backend/src/schema/cryptocurrency.schema.ts
+++ backend/src/schema/cryptocurrency.schema.ts
@@ -1,5 +1,12 @@
 import {Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
 @Schema()
 export class CryptoCurrency{
-
+   @Prop()
+   sign: string;
+   @Prop()
+   currentPriceUSD: number;
+   @Prop()
+   name: string;
+   @Prop()
+   lastUpdated: Date;
 }
