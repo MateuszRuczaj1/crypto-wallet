0374b1d7db7300ab3e6779b0981cfe978186938e
MateuszRuczaj1 <MateuszRuczaj1>
2025-12-02 20:54:50 +0100

    import user modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 // import { ConfigModule } from '@nestjs/config';
 import { CryptoModule } from './crypto/crypto.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL!),
@@ -12,7 +14,7 @@ import { CryptoModule } from './crypto/crypto.module';
     //   isGlobal: true,
     // }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
