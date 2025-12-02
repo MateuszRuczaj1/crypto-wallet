41b3bb2312f4b7fa8391026e8bf0abe8fa25045d
MateuszRuczaj1 <MateuszRuczaj1>
2025-12-02 22:16:10 +0100

    add schemafactory wallet class

--- backend/src/schema/wallet.schema.ts
+++ backend/src/schema/wallet.schema.ts
@@ -4,6 +4,13 @@ import mongoose from "mongoose"
 export class Wallet {
     @Prop({type:mongoose.Schema.Types.ObjectId, ref:'User'} )
     userId: mongoose.Types.ObjectId;
-    @Prop()
-    balances: number
+    @Prop([{
+        currencyId: {type: mongoose.Schema.Types.ObjectId, ref: 'Cryptocurrency'},
+        amount: {type:Number, default: 0}
+    }])
+    balances: {
+    currencyId: mongoose.Types.ObjectId;
+    amount: number;
+     }[];
 }
+export const WalletSchema = SchemaFactory.createForClass(Wallet)
